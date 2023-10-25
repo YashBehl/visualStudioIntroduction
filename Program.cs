23966fc8a2bd4583c48c24530cdab24aec7c671c
YashBehl <147510285+YashBehl@users.noreply.github.com>
2023-10-25 17:05:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstproject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            int x = 25;
+
+            Console.WriteLine(x);
+
+
+        }
+    }
+}
